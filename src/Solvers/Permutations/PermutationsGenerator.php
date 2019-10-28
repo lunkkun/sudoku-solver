26a5567a5c1b890a474db7b0e45cabbfbc79a96e
Luuk Linders <luuk.linders@movements.nl>
2019-10-28 22:13:59 +0100

    Using package permutations-generator

--- src/Solvers/Permutations/PermutationsGenerator.php
+++ src/Solvers/Permutations/PermutationsGenerator.php
@@ -8,23 +8,7 @@ class PermutationsGenerator extends CachedGenerator
 {
     public function __construct(array $values)
     {
-        parent::__construct($this->generator($values));
-    }
-
-    protected function generator(array $values): \Generator
-    {
-        if (count($values) === 1) {
-            yield $values;
-            return;
-        }
-
-        $remaining = [];
-        while (($value = array_pop($values)) !== null) {
-            foreach ($this->generator(array_merge($values, $remaining)) as $permutation) {
-                $permutation[] = $value;
-                yield $permutation;
-            }
-            $remaining[] = $value;
-        }
+        $generator = new \Lunkkun\PermutationsGenerator\PermutationsGenerator();
+        parent::__construct($generator($values));
     }
 }
